496cd82ea9cdf3def13ba148e14f3c2ab29ab44c
damianc <damianczapiewski@gmail.com>
2019-07-29 16:29:52 +0200

    Refactor FilterFilter @filters/

--- src/app/filters/Filter.ts
+++ src/app/filters/Filter.ts
@@ -4,11 +4,15 @@ import { Pipe } from "@angular/core";
 	name: 'filter'
 })
 export class FilterFilter {
+	private contains(str, phrase) {
+		return str.toLowerCase().indexOf(phrase.toLowerCase()) != -1;
+	}
+
 	transform(value: any[], phrase: string, key?: string) {
 		if (phrase == null) return value;
 
-		var filterPureArray = (item) => item.toLowerCase().indexOf(phrase.toLowerCase()) != -1;
-		var filterObjectArray = (item) => item[key].toLowerCase().indexOf(phrase.toLowerCase()) != -1;
+		var filterPureArray = (item) => this.contains(item, phrase);
+		var filterObjectArray = (item) => this.contains(item[key], phrase);
 
 		return value.filter(key == undefined ? filterPureArray : filterObjectArray);
 	}
